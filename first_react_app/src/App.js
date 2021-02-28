36b016bd7c99678a186e7ef771ea619aabceaec5
jaffarabbas <gamaportal8@gmail.com>
2021-02-28 22:01:11 +0500

    2-28-2021     Conditional component practice

--- first_react_app/src/App.js
+++ first_react_app/src/App.js
@@ -9,8 +9,9 @@ const Compi = props => {
       {
         name : 'jaffar',
         age :'21'
-      }
-    ]
+      },
+    ],
+    flag:false
   });
 
   const [NewCom,SetNewCom] = useState("1111");
@@ -22,6 +23,7 @@ const Compi = props => {
         name : NewName,
         age : NewCom
       }]
+      ,flag : true,
     });
   };
 
@@ -30,14 +32,33 @@ const Compi = props => {
       person : [{
         name : event.target.value,
         age : NewCom
-      }]
+      }],
+      flag : true,
     });
   };
 
+  const toggleButton = () => {
+    const Showflag = user.flag;
+    setUser({
+      person:[
+        {
+          name : 'jaffar',
+          age :'21'
+        },
+      ],
+      flag : true,
+    });
+  }
+
   return(
     <div className="App">
-      <Addtion changed={NameChangehandler} name={user.person[0].name}  age={user.person[0].age} click={() => ClickHandler("Ahmed AMin")}>This Is Childern Of Component</Addtion>
-      <button onClick={ClickHandler.bind(this,'NewParamter')}>Click for Handle</button>
+     <button onClick={toggleButton}>Click</button>
+     { user.flag === true?
+      <div>
+        <Addtion changed={NameChangehandler} name={user.person[0].name}  age={user.person[0].age} click={() => ClickHandler("Ahmed AMin")}>This Is Childern Of Component</Addtion>
+        <button onClick={ClickHandler.bind(this,'NewParamter')}>Click for Handle</button>
+      </div> : null
+     }
     </div>
   )
 };
