ac3d528afbc46c453e7ea09d9de9cbfe1680a587
jaffarabbas <gamaportal8@gmail.com>
2021-02-28 19:42:43 +0500

    2-28-2021

--- first_react_app/src/App.js
+++ first_react_app/src/App.js
@@ -1,13 +1,48 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 import Addtion from './newComponent/Addnew'
 
-function App() {
-  return (
-    <div className="App">
-      <Addtion/>
-    </div>
-  );
-}
 
-export default App;
+const Compi = props => {
+  const [user,setUser] = useState( {
+    person:[
+      {
+        name : 'jaffar',
+        age :'21'
+      }
+    ]
+  });
+
+  const [NewCom,SetNewCom] = useState("1111");
+
+  const ClickHandler = (NewName) => {
+    console.log("hello handler");
+    setUser({
+      person : [{
+        name : NewName,
+        age : NewCom
+      }]
+    });
+  };
+
+  const NameChangehandler = (event) => {
+    setUser({
+      person : [{
+        name : event.target.value,
+        age : NewCom
+      }]
+    });
+  };
+
+  return(
+    <div className="App">
+      <Addtion changed={NameChangehandler} name={user.person[0].name}  age={user.person[0].age} click={() => ClickHandler("Ahmed AMin")}>This Is Childern Of Component</Addtion>
+      <button onClick={ClickHandler.bind(this,'NewParamter')}>Click for Handle</button>
+    </div>
+  )
+};
+  
+export default Compi; 
+
+
+
--- first_react_app/src/newComponent/Addnew.js
+++ first_react_app/src/newComponent/Addnew.js
@@ -1,7 +1,14 @@
-import React from './react';
+import React from 'react';
+import './Appnew.css'
 
-const addition = () => {
-    return <p>Jaffar Here</p>
+const addition = (props) => {
+    return (
+        <div className="po">
+             <h1 onClick={props.click}>Hello {props.name} {Math.floor(Math.random()*10/2)} age is {props.age}</h1>
+             <h1>{props.children}</h1>
+             <input type="text" onChange={props.changed}/>
+        </div>
+    )
 };
 
 export default addition;
