652e3c8945ae75af8ebe561942536f4b604b362f
jaffarabbas <gamaportal8@gmail.com>
2021-03-01 20:17:38 +0500

    3-1-2021    Class base Components Practice

--- first_react_app/src/App.js
+++ first_react_app/src/App.js
@@ -1,69 +1,114 @@
-import React, { useState } from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import Addtion from './newComponent/Addnew'
 
 
-const Compi = props => {
-  const [user,setUser] = useState( {
-    person:[
-      {
-        name : 'jaffar',
-        age :'21'
-      },
-    ],
-    flag:false
-  });
+// const Compi = props => {
+//   const [user,setUser] = useState( {
+//     person:[
+//       {
+//         name : 'jaffar',
+//         age :'21'
+//       },
+//     ],
+//     flag:false
+//   });
 
-  const [NewCom,SetNewCom] = useState("1111");
+//   const [NewCom,SetNewCom] = useState("1111");
 
-  const ClickHandler = (NewName) => {
-    console.log("hello handler");
-    setUser({
-      person : [{
-        name : NewName,
-        age : NewCom
-      }]
-      ,flag : true,
-    });
-  };
+//   const ClickHandler = (NewName) => {
+//     console.log("hello handler");
+//     setUser({
+//       person : [{
+//         name : NewName,
+//         age : NewCom
+//       }]
+//       ,flag : true,
+//     });
+//   };
 
-  const NameChangehandler = (event) => {
-    setUser({
-      person : [{
-        name : event.target.value,
-        age : NewCom
-      }],
-      flag : true,
-    });
-  };
+//   const NameChangehandler = (event) => {
+//     setUser({
+//       person : [{
+//         name : event.target.value,
+//         age : NewCom
+//       }],
+//       flag : true,
+//     });
+//   };
 
-  const toggleButton = () => {
-    const Showflag = user.flag;
-    setUser({
-      person:[
-        {
-          name : 'jaffar',
-          age :'21'
-        },
-      ],
-      flag : true,
-    });
-  }
+//   const toggleButton = () => {
+//     const Showflag = user.flag;
+//     setUser({
+//       person:[
+//         {
+//           name : 'jaffar',
+//           age :'21'
+//         },
+//       ],
+//       flag : true,
+//     });
+//   }
 
-  return(
-    <div className="App">
-     <button onClick={toggleButton}>Click</button>
-     { user.flag === true?
-      <div>
-        <Addtion changed={NameChangehandler} name={user.person[0].name}  age={user.person[0].age} click={() => ClickHandler("Ahmed AMin")}>This Is Childern Of Component</Addtion>
-        <button onClick={ClickHandler.bind(this,'NewParamter')}>Click for Handle</button>
-      </div> : null
-     }
-    </div>
-  )
-};
+//   return(
+//     <div className="App">
+//      <button onClick={toggleButton}>Click</button>
+//      { user.flag === true?
+//       <div>
+//         <Addtion changed={NameChangehandler} name={user.person[0].name}  age={user.person[0].age} click={() => ClickHandler("Ahmed AMin")}>This Is Childern Of Component</Addtion>
+//         <button onClick={ClickHandler.bind(this,'NewParamter')}>Click for Handle</button>
+//       </div> : null
+//      }
+//     </div>
+//   )
+// };
   
-export default Compi; 
+class App extends Component {
+  state = {
+    person : [
+      {name : "jaffar", age : '21'}
+    ]
+    ,toogleer : 'check more',
+    flag : false
+  };
+  //onclick
+  ChangeState = () =>{
+    this.setState({
+      person : [
+        {name : "jaffar Abbas", age : '21'}
+      ]
+    })
+  }
+  //runtime changes
+  RuntimeToggle = () =>{
+    this.setState({
+      flag : true
+    })
+  }
+  //name changer
+  NameChanger = (event) =>{
+    this.setState({
+      person : [
+        {name : event.target.value, age : '21'}
+      ]
+      ,toogleer : 'check more',
+      flag : true
+    })
+  }
+  render(){
+    return(
+      <div>
+        {this.state.flag === true ?
+          <div>
+            <Addtion name={this.state.person[0].name} age={this.state.person[0].age} toogle={this.ChangeState} newTogle={this.state.toogleer} changed={this.NameChanger} />
+          </div>:null
+        }
+        <button onClick={this.RuntimeToggle}>Click Toggler</button>
+      </div>
+    )
+  };
+}
+export default App; 
 
 
 
--- first_react_app/src/newComponent/Addnew.js
+++ first_react_app/src/newComponent/Addnew.js
@@ -5,7 +5,8 @@ const addition = (props) => {
     return (
         <div className="po">
              <h1 onClick={props.click}>Hello {props.name} {Math.floor(Math.random()*10/2)} age is {props.age}</h1>
-             <h1>{props.children}</h1>
+             <h1>{props.newTogle}</h1>
+             <button onClick={props.toogle}>Click</button>             
              <input type="text" onChange={props.changed}/>
         </div>
     )
