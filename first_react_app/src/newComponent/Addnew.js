5cba63d82ac775c9977c9c457fa830284b896290
jaffarabbas <gamaportal8@gmail.com>
2021-03-05 03:32:11 +0500

    3-5-2021  Remove Bugs

--- first_react_app/src/newComponent/Addnew.js
+++ first_react_app/src/newComponent/Addnew.js
@@ -11,7 +11,7 @@ const addition = (props) => {
                      <h4 onClick={props.click}> {props.name}</h4>
                     </div>
                     <div className="col-4 col-sm-4 col-md-4">
-                     <input id="form_id" onChange={props.change} className="form-control"  type="text" />
+                     <input id="form_id" onChange={props.change} value={props.name} className="form-control"  type="text" />
                     </div>
                     <div className="col-4 col-sm-4 col-md-4">
                      <button onClick={props.delete} className="btn btn-primary mt-1 ml-2">delete</button>    
