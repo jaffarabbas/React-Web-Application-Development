2721c00ae5b4387e65b59744b9052e7005971df4
jaffarabbas <gamaportal8@gmail.com>
2021-07-03 22:40:50 +0500

    7-3-2021

---
+++ components_practice/src/App.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+import Person from './Components/Person/person';
+
+class App extends Component {
+  render() {
+    return(
+      <Person/>
+    )
+  }
+}
+
+export default App;
---
+++ components_practice/src/Components/Person/person.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const person = (props) => {
+    return(
+        <div>
+            <h4>React</h4>
+        </div>
+    )
+}
+
+export default person;
