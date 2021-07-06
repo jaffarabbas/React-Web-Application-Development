6f58d9d48ee8796ec9731d22478d92078a504bea
jaffarabbas <gamaportal8@gmail.com>
2021-07-06 22:03:43 +0500

    7-6-2021

--- list-practice/src/App.js
+++ list-practice/src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import NumberList from './NumberList'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  render() {
+    const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+    const numbers2 = [1, 2, 33, 43, 54, 65, 7, 8, 9]
+    const todo = [
+      { id: 1, task: 'Jaffar', status: false },
+      { id: 2, task: 'works', status: true }
+    ]
+    return (
+      <div>
+        <NumberList numbers={numbers} />
+        <NumberList numbers={numbers2} />
+        <ul>
+          {todo.map((todo)=>(
+            <li key={todo.id}>
+                {todo.task} - {todo.status?'Done':''}
+            </li>
+          ))}
+        </ul>
+      </div>
+    )
+  }
 }
-
-export default App;
