1a93fd3257d8dcf0c0770d92ecf458fafed0087b
jaffarabbas <gamaportal8@gmail.com>
2021-07-06 22:42:53 +0500

    7-6-2021

--- list-practice/src/NumberList.js
+++ list-practice/src/NumberList.js
@@ -1,8 +1,7 @@
 function NumberList(props) {
-    const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9]
     return (
         <ul>
-            {numbers.map((number, index) => <li key={index}>{number}</li>)}
+            {props.numbers.map((number, index) => <li key={index}>{number}</li>)}
         </ul>
     )
 }
