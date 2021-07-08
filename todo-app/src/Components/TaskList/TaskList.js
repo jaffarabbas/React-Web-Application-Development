52966bbbe84ed7c4fed8099b2ec4cdf8b70bf130
jaffarabbas <gamaportal8@gmail.com>
2021-07-08 21:48:02 +0500

    7-8-2021

---
+++ todo-app/src/Components/TaskList/TaskList.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class TaskList extends Component {
+    render() {
+        return (
+            <div>
+                <h1>List of tasks</h1>
+            </div>
+        )
+    }
+}
