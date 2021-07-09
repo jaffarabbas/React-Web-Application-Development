9a7c9667478053748e477545f89951fd5887f059
jaffarabbas <gamaportal8@gmail.com>
2021-07-09 15:30:54 +0500

    7-9-2021

--- todo-app/src/Components/TaskList/TaskList.js
+++ todo-app/src/Components/TaskList/TaskList.js
@@ -1,11 +1,30 @@
 import React, { Component } from 'react'
+import TaskItem from './TaskItem'
+import "../../../node_modules/bootstrap/dist/css/bootstrap.min.css"
 
 export default class TaskList extends Component {
     render() {
         return (
-            <div>
-                <h1>List of tasks</h1>
-            </div>
+            <table className="table">
+                <thead>
+                    <tr>
+                        <th>Task</th>
+                        <th>Actions</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {this.props.tasks.map((task,index) => (
+                        <TaskItem 
+                            key={index} 
+                            taskItem={task} 
+                            id={index} 
+                            deleteTask={this.props.deleteTask} 
+                            editTask={this.props.editTask}
+                            toggleTask = {this.props.toggleTask}    
+                        />
+                    ))}
+                </tbody>
+            </table>
         )
     }
 }
