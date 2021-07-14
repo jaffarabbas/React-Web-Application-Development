79d47d06d63478bad9462b3586c04255f8205e2b
jaffarabbas <gamaportal8@gmail.com>
2021-07-14 16:03:14 +0500

    7-14-2021       useContext Added

---
+++ hooks-practice/src/useContext/Main.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import ComponentA from '../useContext/Components/Simple Contexting/ComponentA';
+
+export default function Main() {
+    return (
+        <ComponentA/>
+    )
+}
