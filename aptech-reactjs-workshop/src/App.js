8eea3ba8905a4cffb88165a3bfa4810f3e5300f8
jaffarabbas <gamaportal8@gmail.com>
2021-10-13 22:40:23 +0500

    13/10/2021 22:40:23

--- aptech-reactjs-workshop/src/App.js
+++ aptech-reactjs-workshop/src/App.js
@@ -1,15 +1,13 @@
 import React,{useState,useEffect} from 'react'
-
-const incremental = () => {
-  setstate(32);
-}
-
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import dataPassing from './Components/dataPassing';
 export default function App() {
   const [state, setstate] = useState(0);
   return (
     <div>
       <h1>Helo{state}</h1>
-      <button onClick={incremental}>Click me</button>
+      <button className="btn btn-primary ">Click me</button>
+      <dataPassing title = 'hello'/>
     </div>
   )
 }
