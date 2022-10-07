fec3b2562b68660d881c499a5a9acbe2f2666cfc
Jaffar Abbas <54433038+jaffarabbas@users.noreply.github.com>
2022-10-07 13:18:42 +0500

    Inserted src/data/data.js at 07/10/2022 13:18:42

---
+++ react-shopping-cart/src/data/data.js
@@ -0,0 +1,27 @@
+const data = {
+    products:[
+        {
+            _id: '1',
+            title: 'Airpods Wireless',
+            image: 'https://images.unsplash.com/photo-1610438235354-a6ae5528385c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80',
+            description: 'Bluetooth technology lets you connect it with compatible devices wirelessly High-quality AAC audio offers immersive listening experience Built-in microphone allows you to take calls while working',
+            price: 89.99,
+        },
+        {
+            _id: '2',
+            title: 'iPhone 11 Pro 256GB',
+            image: 'https://images.unsplash.com/photo-1571380401583-72ca84994796?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=464&q=80',
+            description: 'Introducing the iPhone 11 Pro. A transformative triple-camera system that adds tons of capability without complexity. An unprecedented leap in battery life',
+            price: 599.99,
+        },
+        {
+            _id: '3',
+            title: 'Cannon EOS 80D Camera',
+            image: 'https://images.unsplash.com/photo-1516852294404-5423eaa0d4a9?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80',
+            description: 'Characterized by versatile imaging specs, the Canon EOS 80D further clarifies itself using a pair of robust focusing systems and an intuitive design',
+            price: 929.99,
+        }
+    ]
+}
+
+export default data;
