6069b474731591493130e0f7b624aa84a561052d
Jaffar Abbas <54433038+jaffarabbas@users.noreply.github.com>
2022-10-09 13:21:01 +0500

    Updated src/App.js at 09/10/2022 13:21:01

--- react-hooks-practice/src/App.js
+++ react-hooks-practice/src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import Api from './useEffect/Api';
 import Counter from './useState/counter';
 import Textfeild from './useState/textfeild';
 
@@ -6,7 +7,7 @@ function App() {
   return (
     <div className="App">
      <h1>Hooks</h1>
-     <Textfeild/>
+     <Api/>
     </div>
   );
 }
