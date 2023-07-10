cddbb62b45d4c7bff5dcf9469ad53f444166c1c7
jaffarabbas <gamaportal8@gmail.com>
2023-07-10 22:23:07 +0500

    Commited File : Video.js at 10/07/2023 10:23:07 pm

--- react-master-class/src/components/Video.js
+++ react-master-class/src/components/Video.js
@@ -1,9 +1,11 @@
 import React from 'react'
+import Button from './Button'
 
 export default function Video({title,flag}) {
   return (
     <>
         <div>{title} {flag && 'hello flagger'}</div>
+        <Button message="Hello">Click</Button>
     </>
   )
 }
