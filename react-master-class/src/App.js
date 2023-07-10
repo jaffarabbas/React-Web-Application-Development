1974715340b6c1e69f3e9c3c86fa457831816017
jaffarabbas <gamaportal8@gmail.com>
2023-07-10 21:00:18 +0500

    Commited File : App.js at 10/07/2023 9:00:18 pm

--- react-master-class/src/App.js
+++ react-master-class/src/App.js
@@ -2,13 +2,26 @@ import './App.css';
 import Video from './components/Video';
 
 function App() {
+  let arr = [
+    {
+      title: 'jaffar',
+      flag: true
+    },
+    {
+      title: 'jaffar1',
+      flag: false
+    },
+    {
+      title: 'jaffar2',
+      flag: true
+    },
+  ];
   return (
    <div>
     Hello 
-    <Video title="jaffar"/>
-    <Video title="jaffar1"/>
-    <Video title="jaffar2"/>
-    <Video title="jaffar3" flag={true}/>
+    {
+      arr.map((item,index) => <Video key={index} title={item.title} flag={item.flag}/>)
+    }
    </div>
   );
 }
