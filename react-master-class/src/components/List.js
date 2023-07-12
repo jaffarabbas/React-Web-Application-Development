b129c19f849274e91bdb5dca446d4fa0525101d9
jaffarabbas <gamaportal8@gmail.com>
2023-07-12 12:43:17 +0500

    Commited File : List.js at 12/07/2023 12:43:17 pm

---
+++ react-master-class/src/components/List.js
@@ -0,0 +1,21 @@
+import React from "react";
+import Video from "./Video";
+
+function List({ video, deleteVideo, editVideo }) {
+  return (
+    <>
+      {video.map((item, index) => (
+        <Video
+          key={index}
+          id={index}
+          title={item.title}
+          flag={item.flag}
+          deleteVideo={deleteVideo}
+          editVideo={editVideo}
+        />
+      ))}
+    </>
+  );
+}
+
+export default List;
