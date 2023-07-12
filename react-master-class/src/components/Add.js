2c1f0eb6bd6e6d17a4cd31c6ca67b27e5196ef5c
jaffarabbas <gamaportal8@gmail.com>
2023-07-12 22:16:40 +0500

    Commited File : Add.js at 12/07/2023 10:16:40 pm

--- react-master-class/src/components/Add.js
+++ react-master-class/src/components/Add.js
@@ -1,20 +1,20 @@
 import React, { useEffect, useState } from 'react'
 let schema = {
     title: '',
-    flag: false
+    flag: ''
 };
 
 function Add({add,update,edit}) {
     const [obj,setObj] = useState(schema);
 
     function handleSubmit(e) {
-        // e.preventDefault();
-        // if(edit){
-        //     update(obj);
-        // }else{
-        //     add(obj);
-        // }
-        // setObj(schema);
+        e.preventDefault();
+        if(edit){
+            update(obj);
+        }else{
+            add(obj);
+        }
+        setObj(schema);
     }
 
     function handleChange(e) {
@@ -31,7 +31,7 @@ function Add({add,update,edit}) {
   return (
     <div>
         <input type='text' name='title' value={obj.title} onChange={handleChange} placeholder='Title'/>
-        <input type='checkbox' name='flag' checked={obj.flag} onChange={handleChange} placeholder='Flag'/>
+        <input type='text' name='flag' value={obj.flag} onChange={handleChange} placeholder='Flag'/>
         <button onClick={handleSubmit}>{edit ? 'Edit' : 'Add'}</button>
     </div>
   )
