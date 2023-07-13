e6879adbde5b2c6cfb56ade6e68f6ead2061ef0f
jaffarabbas <gamaportal8@gmail.com>
2023-07-13 22:08:53 +0500

    Commited File : Video.js at 13/07/2023 10:08:52 pm

--- react-master-class/src/components/Video.js
+++ react-master-class/src/components/Video.js
@@ -1,12 +1,12 @@
 import React from 'react'
 import CLickButton from './Button'
 
-export default function Video({title,flag,deleteVideo,editVideo,id}) {
+export default function Video({title,flag,dispatch,editVideo,id}) {
   return (
     <>
         <div>{title} {flag && 'hello flagger'}</div>
         <CLickButton message="Hello" onClick={()=>console.log('hellll')}>Click</CLickButton>
-        <button onClick={() => deleteVideo(id)}>Delete</button>
+        <button onClick={()=>dispatch({type: 'DELETE',payload: id})}>Delete</button>
         <button onClick={() => editVideo(id)}>Edit</button>
     </>
   )
