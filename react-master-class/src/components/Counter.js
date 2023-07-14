6d51b7fd6e7fe18d2b240239d0d8512e068d7187
jaffarabbas <gamaportal8@gmail.com>
2023-07-14 19:19:55 +0500

    Commited File : Counter.js at 14/07/2023 7:19:55 pm

--- react-master-class/src/components/Counter.js
+++ react-master-class/src/components/Counter.js
@@ -1,9 +1,12 @@
-import React, { useState } from 'react'
+import React, { useRef, useState } from 'react'
 
 function Counter() {
     let [number,setNumber] = useState(0);
+    const num = useRef(0);
+
     function handleClick() {
         setNumber(number+1);
+        num.current++;
         console.log(number);
     }
   return (
