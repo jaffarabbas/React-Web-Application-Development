a4e430378a674e998a9c60c394a2af9b1da8cf6a
jaffarabbas <gamaportal8@gmail.com>
2023-07-17 20:31:37 +0500

    Update List.js

--- react-master-class/src/components/List.js
+++ react-master-class/src/components/List.js
@@ -5,32 +5,21 @@ import axios from "axios";
 import CLickButton from "./Button";
 import useVideoDispatch from "../hooks/VideoDispatch";
 function List({ editVideo }) {
-  // const video = useVideoContext();
-  // const dispatch = useVideoDispatch();
-
-  const [video,setVideo] = useState([]);
+  const video = useVideoContext();
+  const dispatch = useVideoDispatch();
   const url = "http://localhost:5000/users";
 
   async function handleClick(){
     const data = await axios.get(url);
     console.log(data);
-    setVideo(data.data);
-    // dispatch({ type: "LOAD", payload: data.data });
+    dispatch({ type: "LOAD", payload: data.data });
   }
   useEffect(() => {
     handleClick();
   }, []);
   return (
     <>
-      {
-        video.map((item, index) => {
-          return <div key={item.id}>
-          <p>{item.title}</p>
-          <p>{item.flag ? 'true' : 'false'}</p>
-          </div>
-        })
-      }
-      {/* {video.map((item, index) => (
+      {video.map((item, index) => (
         <Video
           key={index}
           id={item.id}
@@ -38,7 +27,7 @@ function List({ editVideo }) {
           flag={item.flag}
           editVideo={editVideo}
         />
-      ))} */}
+      ))}
       <br></br>
       <CLickButton onClick={handleClick}>Fetch</CLickButton>
     </>
