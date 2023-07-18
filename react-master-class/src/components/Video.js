6960abe839fe3f300ce8b0cad6b9ac3896f6f494
jaffarabbas <gamaportal8@gmail.com>
2023-07-18 13:21:13 +0500

    Commited File : Video.js at 18/07/2023 1:21:13 pm

--- react-master-class/src/components/Video.js
+++ react-master-class/src/components/Video.js
@@ -1,15 +1,25 @@
-import React, { useContext } from 'react'
+import React, { memo, useCallback, useContext } from 'react'
 import CLickButton from './Button'
 import useVideoDispatch from '../hooks/VideoDispatch';
 
-export default function Video({title,flag,editVideo,id}) {
-  const dispatch = useVideoDispatch();
-  return (
-    <>
-        <div>{title} {flag && 'hello flagger'}</div>
-        <CLickButton message="Hello" onClick={()=>console.log('hellll')}>Click</CLickButton>
-        <button onClick={()=>dispatch({type: 'DELETE',payload: id})}>Delete</button>
-        <button onClick={() => editVideo(id)}>Edit</button>
-    </>
-  )
-}
+//component memoization
+//i use it because every time i click edit the Video component in list are renderd again e.g 
+//i have 10 video in list i click in one video for edit rest of 9 will also be renderd falto me
+//so i use memoization to prevent this
+const Video = memo(
+  function Video({title,flag,editVideo,id}) {
+    console.log('video rendered');
+    const dispatch = useVideoDispatch();
+    const click = useCallback(()=>console.log('hellll'),[]);
+    return (
+      <>
+          <div>{title} {flag && 'hello flagger'}</div>
+          <CLickButton message="Hello" onClick={click}>Click</CLickButton>
+          <button onClick={()=>dispatch({type: 'DELETE',payload: id})}>Delete</button>
+          <button onClick={() => editVideo(id)}>Edit</button>
+      </>
+    )
+  }
+);
+
+export default Video
