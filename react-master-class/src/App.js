c5e01a12f87b4a571f03319f40b9ea0035511619
jaffarabbas <gamaportal8@gmail.com>
2023-07-18 15:17:12 +0500

    Commited File : App.js at 18/07/2023 3:17:11 pm

--- react-master-class/src/App.js
+++ react-master-class/src/App.js
@@ -1,4 +1,4 @@
-import { useCallback, useContext, useReducer, useState } from "react";
+import { useCallback, useContext, useReducer, useRef, useState } from "react";
 import "./App.css";
 import Counter from "./components/Counter";
 import Add from "./components/Add";
@@ -7,11 +7,14 @@ import ThemeContext from "./context/ThemeContext";
 import CLickButton from "./components/Button";
 import VideoContext from "./context/VideoContext";
 import VideoDispatchContext from "./context/VideoDispaatchContext";
+import Dom from "./components/Dom";
 
 function App() {
   const [edit, setEdit] = useState(null);
   const [video, dispatch] = useReducer(videoReduser, []);
   const [mode, setMode] = useState("dark");
+  const intputRef = useRef(null);
+
   function videoReduser(video, action) {
     switch (action.type) {
       case "LOAD":
@@ -38,23 +41,23 @@ function App() {
   //it is constant it will never change
   // const themeContext = useContext(ThemeContext);
   // console.log(themeContext);
-
- 
-
   const theme = useCallback( function changeTheme() {
     setMode(mode === "dark" ? "light" : "dark");
   },[mode]);
+
   return (
     <ThemeContext.Provider value={mode}>
       <VideoContext.Provider value={video}>
         <VideoDispatchContext.Provider value={dispatch}>
           <div className={`App ${mode}`}>
             <CLickButton onClick={theme}>Theme</CLickButton>
+            <Dom ref={intputRef}/>
+            <CLickButton onClick={()=>intputRef.current.customFocus()}>CLick For Ref</CLickButton>
             <List editVideo={editVideo}></List>
             <br />
             <Add edit={edit}></Add>
             <br/>
-          <Counter></Counter>
+          {/* <Counter></Counter> */}
           </div>
         </VideoDispatchContext.Provider>
       </VideoContext.Provider>
